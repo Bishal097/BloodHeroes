2e66bac4b554f357538a244c07546b2bb082bd75
Bishal Bhattacharjee <bishalbhattacharjee55@gmail.com>
2025-06-05 15:02:43 +0530

    fianl

--- src/components/CarouselSection.jsx
+++ src/components/CarouselSection.jsx
@@ -40,11 +40,15 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Dynamic image component
+// Import images from src/assets/images/
+import image6 from '../assets/images/image6.jpg';
+import image7 from '../assets/images/image7.jpg';
+import image8 from '../assets/images/image8.png';
+
 const ExampleCarouselImage = ({ imgSrc, text }) => (
   <img
     className="d-block w-100"
-    src={`${import.meta.env.BASE_URL}${imgSrc}`} // Correctly resolves to /BloodHeroes/image6.jpg on GitHub Pages
+    src={imgSrc}
     alt={text}
     style={{ height: '100%', maxHeight: '100vh', objectFit: 'contain' }}
   />
@@ -53,19 +57,19 @@ const ExampleCarouselImage = ({ imgSrc, text }) => (
 const CarouselSection = () => (
   <Carousel slide={false} style={{ width: '100%', margin: 'auto' }}>
     <Carousel.Item>
-      <ExampleCarouselImage imgSrc="image6.jpg" text="First slide" />
+      <ExampleCarouselImage imgSrc={image6} text="First slide" />
       <Carousel.Caption>
         <h3>First slide label</h3>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <ExampleCarouselImage imgSrc="image7.jpg" text="Second slide" />
+      <ExampleCarouselImage imgSrc={image7} text="Second slide" />
       <Carousel.Caption>
         <h3>Second slide label</h3>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <ExampleCarouselImage imgSrc="image8.png" text="Third slide" />
+      <ExampleCarouselImage imgSrc={image8} text="Third slide" />
       <Carousel.Caption>
         <h3>Third slide label</h3>
       </Carousel.Caption>
